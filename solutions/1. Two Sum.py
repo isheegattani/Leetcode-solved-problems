606797dd98136c86fecfaa97739160d7866cbd5a
Ishee Gattani <61261197+isheegattani@users.noreply.github.com>
2021-01-24 11:58:37 -0800

    48 ms

---
+++ solutions/1. Two Sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in nums and nums.index(diff) != i:
+                    return [i, nums.index(diff)]
+          
+            
+                
+           
+            
+        
